5f1428f03858f15cd143df14b09caa4bfb006fb8
Tu250805 <Tungu0166@gmail.com>
2025-04-11 08:58:29 +0700

    Add files via upload

---
+++ bai5/cau5-3.py
@@ -0,0 +1,7 @@
+print("Sinh Vien : Đậu Đức Tú")
+print("MSSV:235752020710009")
+import numpy as np
+# Tạo một mảng với các giá trị từ 12 đến 38
+x = np.arange(12, 38)
+# In mảng ra màn hình
+print(x)
