61d1941364e21fe20887f3f8e6c3e79f42f0f1be
Tu250805 <Tungu0166@gmail.com>
2025-04-11 08:19:31 +0700

    Add files via upload

---
+++ bai 4/bai 4.4t.py
@@ -0,0 +1,9 @@
+print("sinh vien : Dau Duc Tu")
+print("msv 235752020710009")
+s = input('Danh sách: ').split()
+# In cả dãy vừa nhập
+print(ds)
+
+# In dãy vừa nhập, mỗi phần tử trên một dòng
+for so in ds:
+    print(so)
